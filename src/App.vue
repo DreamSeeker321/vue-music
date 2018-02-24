9c092967cc632474f23bb03c2c164981348f38ec
yb <737615014@qq.com>
2018-02-24 17:14:11 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    hello world
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  #app
+    color: $color-theme
+</style>
